3e78519344de91cd47e1e63c2f4edcfa0cff2896
JuanDev <carlino0215@gmail.com>
2024-06-03 19:46:35 -0400

    Terea semana 8

---
+++ app/src/main/java/com/cursokotlin/notificationexample/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.cursokotlin.notificationexample
+
+import android.annotation.SuppressLint
+import android.app.AlarmManager
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.cursokotlin.notificationexample.AlarmNotification.Companion.NOTIFICATION_ID
+import java.util.Calendar
+
+class MainActivity : AppCompatActivity() {
+    companion object {
+        const val MY_CHANNEL_ID = "myChannel"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val myNotificationButton = findViewById<Button>(R.id.btnNotification)
+        createChannel()
+        myNotificationButton.setOnClickListener {
+            scheduleNotification()
+        }
+    }
+
+    @SuppressLint("ScheduleExactAlarm")
+    private fun scheduleNotification() {
+        val intent = Intent(applicationContext, AlarmNotification::class.java)
+        val pendingIntent = PendingIntent.getBroadcast(
+            applicationContext,
+            NOTIFICATION_ID,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, Calendar.getInstance().timeInMillis + 120000, pendingIntent)
+    }
+
+    private fun createChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                MY_CHANNEL_ID,
+                "MySuperChannel",
+                NotificationManager.IMPORTANCE_DEFAULT
+            ).apply {
+                description = "SUSCRIBETE"
+            }
+
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+}
